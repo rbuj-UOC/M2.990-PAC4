f03b1d10bbf18a1d16a32670b290332b758d4c36
Robert Buj <rbuj@users.noreply.github.com>
2025-12-30 11:17:53 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Fourth Assignment
+
+## Install Required Libraries
+
+To install the required R libraries, run the following command in your R
+console:
+
+```r
+source("install.R")
+```
+
+To install Pandoc on macOS, use Homebrew:
+
+```shell
+brew install pandoc
+```
---
+++ install.R
@@ -0,0 +1,17 @@
+# Install required packages for R-cast course
+install.packages(c(
+  "car", # for regression diagnostics
+  "caret", # for confusion matrix and other ML utilities
+  "data.table", # for efficient data manipulation
+  "kableExtra", # for enhanced table formatting
+  "corrplot", # for correlation plots
+  "dplyr", # for data manipulation
+  "ggplot2", # for data visualization
+  "gridExtra", # for arranging multiple ggplots
+  "knitr", # for dynamic report generation
+  "languageserver", # for R language server support for VSCode
+  "nortest", # for normality tests
+  "pROC", # for ROC curve analysis
+  "rmarkdown", # for dynamic report generation
+  "tidyr" # for data tidying
+))
